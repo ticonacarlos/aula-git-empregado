5748c1d5c6a8fa4477508f3cc5337fa58e38c87a
‘carlos’ <carlos.diegoticona@yahoo.com>
2023-03-20 20:56:37 -0300

    Calculo de salário efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,7 +1,20 @@
 public class Empregado {
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario, comissao, bonus;
 
+    public double calculaSalario(){
+        if (tipo == 1){
+            return salario;
+        } else if (tipo == 2){
+            return salario + salario * comissao;
+        } else if (tipo == 3){
+            return salario + bonus;
+        } else {
+            return 0.0;
+        }
+    }
 
     public String getNome() {
         return this.nome;
