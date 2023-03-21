784cca0df458f67ac05f7de82c0d6b5c722e7986
‘carlos’ <carlos.diegoticona@yahoo.com>
2023-03-20 21:18:04 -0300

    Primeiro teste nao concluido totalmente, pois nao possuia compilador instalado no momento

--- Empregado.java
+++ Empregado.java
@@ -40,4 +40,22 @@ public class Empregado {
         this.tipo = tipo;
     }
     
+    public void setSalario (double salario){
+        this.salario = salario;
+        }
+    public double getSalario (){
+        return this.salario;
+        }
+    public void setComissao (double comissao){
+    this.comissao = comissao;
+        }
+        public double getComissao (){
+        return this.comissao;
+        }
+        public void setBonus (double bonus){
+        this.bonus = bonus;
+        }
+        public double getBonus (){
+        return this.bonus;
+        }
 }
--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -4,5 +4,21 @@ public class TesteEmpregado {
         Empregado e1 = new Empregado();
         Empregado e2 = new Empregado();
         Empregado e3 = new Empregado();
+    
+        e1.setTipo(tipo:1);
+        
+        e2.setTipo(tipo:2);
+        
+        e3.setTipo(tipo:3);
+
+        e1.setSalario(salario:2000)
+        e2.setSalario(salario:1700)
+        e2.setComissao (0.2);//20% de comissão sobre o salário
+        e3.setSalario(1500);
+        e3.setBonus(400);
+        //mostra os valores
+        System.out.println (e1.calculaSalario());
+        System.out.println (e2.calculaSalario());
+        System.out.println (e3.calculaSalario());
     }
 }
