2821bc15d43dcd7bc0b7b2ccbaf0085ead6e6cfa
‘carlos’ <carlos.diegoticona@yahoo.com>
2023-03-27 19:56:56 -0300

    Processamento de empregados usando lista

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -16,9 +16,14 @@ public class TesteEmpregado {
         e2.setComissao (0.2);//20% de comissão sobre o salário
         e3.setSalario(1500);
         e3.setBonus(400);
+        //adcionando todo mundo em uma lista        
+        List <Empregado> emps = arrays.asList(e1, e2, e3);
         //mostra os valores
-        System.out.println (e1.calculaSalario());
-        System.out.println (e2.calculaSalario());
-        System.out.println (e3.calculaSalario());
+        //    System.out.println (e1.calculaSalario());
+        //    System.out.println (e2.calculaSalario());
+        //    System.out.println (e3.calculaSalario());
+        for (Empregado e: emps) {
+            System.out.println(e.calculaSalario());
+        }
     }
 }
